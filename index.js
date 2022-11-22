5035d62cc1e6feb3cc859a318879a007aa5a585c
Allan Monzon <greengamer.am@gmail.com>
2022-11-22 14:52:20 -0500

    commit tree 24cea95756c1e6f8414ccee77bded23b1de95ea4

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser ='Allan Monzon-Do Amaral'
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+//Welcome to Flatbook'TM
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting =`Welcome, ${currentUser.slice(0, 1)}!`
